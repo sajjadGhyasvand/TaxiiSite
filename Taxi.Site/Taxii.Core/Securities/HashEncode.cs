d2c40a07d9530bacb8317ae644200a38611fb2f8
sajjadGh1995@hotmail.com <sajjadGh1995@hotmail.com>
2024-01-11 16:40:26 +0330

    Hash Password and Shamsi DateTime Class

---
+++ Taxi.Site/Taxii.Core/Securities/HashEncode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxii.Core.Securities
+{
+    public static class HashEncode
+    {
+        public static string GetHashCode(string password)
+        {
+            Byte[] mainBytes;
+            Byte[] encodeBytes;
+
+            MD5 md5;
+            md5 = new MD5CryptoServiceProvider();
+            mainBytes = ASCIIEncoding.Default.GetBytes(password);
+            encodeBytes = md5.ComputeHash(mainBytes);
+            return BitConverter.ToString(encodeBytes);
+        }
+    }
+}
